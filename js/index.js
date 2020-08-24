c2fbb5ddc55f651f8151f30a78801c64dbfefc6a
auliafaizahr <zahaulia@gmail.com>
2020-08-24 15:25:27 +0700

    add folder js and its subfolder

---
+++ js/index.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+import {  } from "../node_modules/popper.js/dist/popper.min";
+import {  } from "../node_modules/bootstrap/dist/js/bootstrap.min";
+
+import {  } from "../js/vendors/simple-jekyll-search.min";
+import {  } from "../js/search_script";
+
+import {  } from "../js/vendors/transition";
+import {  } from "../js/vendors/zoom.min";
+import {  } from "../js/vendors/instafeed.min";
+import {  } from "../js/vendors/jquery.fitvids";
+import {  } from "../node_modules/animate.css/animate.min.css";
+
+
+import 'devicon/devicon.css';
+import 'devicon/devicon-colors.css';
+
+import "devicon/fonts/devicon.eot";
+import "devicon/fonts/devicon.woff";
+
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+
+import '@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff';
+import '@fortawesome/fontawesome-free/webfonts/fa-brands-400.woff2';
+import '@fortawesome/fontawesome-free/webfonts/fa-brands-400.eot';
+import '@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff';
+import '@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff';
+
+import '../_includes/main.scss';
